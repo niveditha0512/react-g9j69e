464f3bc83e9149463a8b3824f0db190572a2f201
niveditha0512 <disisa.t.nive@gmail.com>
2022-07-01 19:33:25 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
 ReactDOM.render(
   <div>
-    <h1>Hello World !</h1>
-    <p>This is a paragraph</p>
+    <h1>My favourite Super Heroes!</h1>
+    <h3>
+      <ul>
+        <li>Spider-Man</li>
+        <li>Thor</li>
+        <li>Iorn-Man</li>
+      </ul>
+    </h3>
   </div>,
   document.getElementById('root')
 );
+*/
+/*
+const fname = 'Niveditha',
+  lname = 'AT',
+number1 = 5;
+ReactDOM.render(
+  <div>
+    <h1>Hello my name is {fname + ' ' + lname}</h1>
+    <p>My lucky number is {number1}</p>
+  </div>,
+  document.getElementById('root')
+);
+*/
+
+ReactDOM.render(
+<
+
+
+)
